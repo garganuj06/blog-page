b6a2cb8704fb95fef76169c1ed268f473b027f90
garganuj06 <anujgarg0602@gmail.com>
2024-10-19 19:19:24 +0530

    redux added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-
+import React,{useState,useEffect} from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import {login,logout} from "./redux/authSlice"
+import authservice from './Appwrite/auth'
+import Header from './comp/Header'
+import Footer from './comp/Footer'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-    
-    <h1>Vite hello</h1>
-    </>
-  )
+  // console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+
+  const dispatch=useDispatch()
+
+  useEffect(()=>{
+    authservice.getUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login(userData))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(setLoading(false))
+  },[])
+
+  return loading?(<div>Loading...</div>):(
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+    <div className='w-full h-screen block'>
+      <Header/>
+      <Footer/>
+    </div>
+  </div>)
 }
 
 export default App
